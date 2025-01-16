e2b83d900edaf08df302ed2ca665e5c552a0c192
kane <kane@kanes-Air.lan>
2025-01-15 18:51:55 -0800

    feat: navbar basic functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,21 @@
+import { NAV_LINKS } from "@/constants"
 import Image from "next/image"
 import Link from "next/Link"
 
 const Navbar = () => {
     return (
-        <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+        <nav className="flexBetween max-container padding-container relative z-30 py-5">
             <Link href="/">
-                Home
+            <Image src="hilink-logo.svg" alt="logo" width={74} height={29}/>
             </Link>
+
+            <ul className="hidden h-full gap-12 lg:flex">
+                {NAV_LINKS.map((link) => (
+                <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+                    {link.label}
+                </Link>
+                ))}
+            </ul>
         </nav>
     )
 }
